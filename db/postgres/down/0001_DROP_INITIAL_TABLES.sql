6dc592bf061e622ddd62f46829f99b1ecab02ccb
Curtis Smith <Curtis.MBSmith@gmail.com>
2016-03-12 13:25:29 -0400

    HNIN-0: Some cleanup.

--- db/postgres/down/0001_DROP_INITIAL_TABLES.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS hnin.household_users;
-DROP TABLE IF EXISTS hnin.tasks;
-DROP TABLE IF EXISTS hnin.households;
-DROP TABLE IF EXISTS hnin.users;
-DROP TYPE IF EXISTS hnin.granularity;
